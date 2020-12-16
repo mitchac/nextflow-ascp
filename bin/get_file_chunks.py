b12bbe83ca06fd22ddadf0a43c5426a45877d1d4
mitchac <mitchac@gmail.com>
2020-12-16 11:54:04 +1000

    Update get_file_chunks.py

--- bin/get_file_chunks.py
+++ bin/get_file_chunks.py
@@ -3,7 +3,7 @@ import sys
 import csv
 file_path = sys.argv[1]
 file_size = int(sys.argv[2])
-chunk_size = 1000000
+file_size = int(sys.argv[3])
 rng = range(0, file_size, chunk_size)
 file_chunks = [[file_path,x,min(file_size, x+chunk_size),len(rng)] for x in rng] 
 with open("out.txt", "w", newline="") as f:
